805d7f46545de3792279d29fb7b7e4d25e757ba8
dvichinyan <Edlanta301>
2021-05-18 11:30:09 +0300

    Edit profile service method implemented and edit profile component.

--- src/app/components/profile/edit-profile/edit-profile.component.ts
+++ src/app/components/profile/edit-profile/edit-profile.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProfileRequest } from '../payload/profile.request';
+import { ProfileResponse } from '../payload/profile.response';
+import { ProfileService } from '../services/profile-service';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss']
 })
-export class EditProfileComponent implements OnInit {
+export class EditProfileComponent implements OnInit, OnDestroy  {
 
-  constructor() { }
+  public editProfileUser: ProfileRequest = new ProfileRequest();
+  
+  private subscription = new Subscription();
+
+  public profile: ProfileResponse = {
+    firstName: '',
+    lastName: '',
+    bornOn: new Date(),
+    email: ''
+  };
+  
+  constructor(
+    private profileService: ProfileService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.profileService.viewProfile().subscribe(response => {
+      this.profile = response;
+    });
   }
 
+  editProfile(): void {
+    this.profileService.editProfile(this.editProfileUser).subscribe(response => {
+      this.profile = response,
+      this.router.navigate(['/view-profile'])
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/components/profile/services/profile-service.ts
+++ src/app/components/profile/services/profile-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { ProfileRequest } from "../payload/profile.request";
 import { ProfileResponse } from "../payload/profile.response";
 
 @Injectable({providedIn: 'root'})
@@ -14,5 +15,9 @@ export class ProfileService {
 
     viewProfile(): Observable<ProfileResponse> {
         return this.http.get<ProfileResponse>(environment.profileApiUrl + '/me');
+    }
+
+    editProfile(editProfileRequest: ProfileRequest): Observable<ProfileResponse> {
+        return this.http.patch<ProfileResponse>(environment.profileApiUrl + '/me', editProfileRequest);
     }
 }
